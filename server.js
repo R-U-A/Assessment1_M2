e3909e2de1a0be489973ca9467b85524e01c3b93
R-U-A <richard.budden@outlook.com>
2022-10-13 11:47:37 +1000

    additional socket

--- server.js
+++ server.js
@@ -1,11 +1,31 @@
+//required modules/packages
 const http = require('http');
 const debug = require("debug")("node-angular");
 const app = require('./chatroom/backend/app');
+const sockets = require("./chatroom/backend/socket");
+const cors = require("cors");
 
 const PORT = 3000;
 
+// io declaration
+
+const io = require("socket.io")(http, {
+  cors: {
+    origin: "http://localhost:4200/chat",
+    methods: ["GET", "POST"],
+  },
+});
+
+
+// set port for app
+
 app.set('port', PORT);
 
+
+//  create server
 const server = http.createServer(app);
 
+
+//listen for connections and requests from front
+sockets.connect(io, PORT);
 server.listen(PORT);
